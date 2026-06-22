0c0bec65ef191e4cdc147904f5b69fd0d9196ed8
yassinsalam671-pixel <yassinsalam671@gmail.com>
2026-06-23 01:48:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 
 export default defineConfig({
-  base: "./",
+  base: "/cavallari-web/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
